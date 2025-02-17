e83228122090598893e673f479387f83e58f3222
Vasanthraja123 <150172085+Vasanthraja123@users.noreply.github.com>
2025-02-18 05:21:31 +0530

    Update server.py

--- server.py
+++ server.py
@@ -660,4 +660,5 @@ def recognize_face():
         })
 
 if __name__ == '__main__':
-     app.run(debug=True)
+       port = int(os.environ.get("PORT", 5000))
+       app.run(host="0.0.0.0", port=port)
